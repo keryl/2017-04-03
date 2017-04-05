3ab26467def6e40a2cc555bcc71027df18ab950f
keryl <kerylklonye@gmail.com>
2017-04-05 19:33:48 +0300

    add add and remove files

---
+++ add.py
@@ -0,0 +1,14 @@
+def add(list, number):
+    list = []
+    list.append(number)
+    return list
+
+
+
+
+
+
+
+
+
+
---
+++ remove.py
@@ -0,0 +1,4 @@
+def remove(list, number):
+    list = []
+    list.remove(number)
+    return list
